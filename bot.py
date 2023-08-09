2ff54a64866a77a4e41e7615e956fd8f3a42750d
Axel Pröger <a.proeger@gmail.com>
2023-08-09 22:47:45 +0200

    env change and reload command

--- bot.py
+++ bot.py
@@ -1,25 +1,28 @@
 import os, nextcord
 from dotenv import load_dotenv
+from os import environ as env
 from nextcord.ext import commands
 from classes.database import Database
-from nextcord import Interaction
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
-GUILD_ID = os.getenv("GUILD_ID")
 
-db = Database("predictions.db")
+TOKEN: str = str(env.get("BOT_TOKEN"))
+GUILD_ID: int = int(env.get("GUILD_ID"))
+
+db = Database("database.db")
 
 intents = nextcord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="$", intents=intents)
+bot.test = "testo"
 
 
 @bot.event
 async def on_ready():
     await db.connect()
+    bot.db = db
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
 
@@ -39,6 +42,15 @@ if __name__ == "__main__":
             print(f"Extension {extension} loaded.")
         except Exception as e:
             print("Error loading extension:", e)
+
+
+@bot.command()
+async def reload(ctx, extension):
+    try:
+        bot.reload_extension(f"cogs.{extension}")
+        await ctx.send(f"Extension {extension} reloaded.")
+    except Exception as e:
+        await ctx.send("Error loading extension:", e)
 
 
 bot.run(TOKEN)
